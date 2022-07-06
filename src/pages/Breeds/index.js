4f5578fc0b4af9a432d35a0a82cc061693d0e299
Caio Garcia <caiopassador@hotmail.com>
2022-07-06 08:34:13 +0100

    about styles

--- src/pages/Breeds/index.js
+++ src/pages/Breeds/index.js
@@ -39,18 +39,31 @@ export function Breeds() {
 
         .map((currentBreed) => {
           return (
-            <Link to={`/breeds/${currentBreed.id}`}>
-              <div style={{ display: "flex" }}>
-                {/* <img src={cE.image_url} alt='beer' style={{ width: '100px' }} /> */}
-                <div style={{ flexDirection: "column" }}>
-                  <p>{currentBreed.name}</p>
+            <>
+              <div key="id" className="card" style={{ width: "18rem" }}>
+                {currentBreed.image === undefined ||
+                currentBreed.image === {} ? (
+                  <span>No image available</span>
+                ) : (
+                  <img
+                    src={currentBreed.image.url}
+                    className="breed-logo"
+                    alt="breed-logo"
+                  />
+                )}
+                <div className="card-body">
+                  <h2 className="card-title">{currentBreed.name}</h2>
+                  <p className="card-text">{currentBreed.description}</p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
                 </div>
               </div>
-            </Link>
+            </>
           );
         })}
 
-      <>
+      {/* <>
         {cats.map((currentElement) => {
           console.log(currentElement.image);
           return (
@@ -66,20 +79,18 @@ export function Breeds() {
                     alt="breed-logo"
                   />
                 )}
-
-                {/* // breeds card */}
                 <div className="card-body">
                   <h2 className="card-title">{currentElement.name}</h2>
                   <p className="card-text">{currentElement.description}</p>
-                  {/* <a href="#" className="btn btn-primary">
+                  <a href="#" className="btn btn-primary">
                   Go somewhere
-                </a> */}
+                </a>
                 </div>
               </div>
             </>
           );
         })}
-      </>
+      </> */}
     </>
   );
 }
